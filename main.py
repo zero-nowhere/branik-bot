cdf48034e43a230a829b6456a9659d38d317f80f
zero-nowhere <52384715+zero-nowhere@users.noreply.github.com>
2021-02-17 14:40:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import config
+import getprices
+
+import requests
+import flask
+
+import telebot
+
+
+app = flask.Flask(__name__)
+
+
+bot = telebot.TeleBot(config.TG_TOKEN)
+
+URL = 'https://api.telegram.org/bot' + config.TG_TOKEN + '/'
+
+@app.route('/' + config.TG_TOKEN, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+	bot.remove_webhook()
+	bot.set_webhook(url="https://{}.herokuapp.com/{}".format(config.APP_NAME, config.TG_TOKEN))
+	return '1'
+
+@bot.message_handler(commands=['branik','Branik'])
+def send_price(message):
+	with open ('akce', 'r') as f:
+		prices = f.read()
+	bot.send_message(message.chat.id, prices, parse_mode='Markdown')
+	f.close()
+
+@bot.message_handler(commands=['refresh'])
+def refresh_price(message):
+	import getprices
+	bot.send_message(message.chat.id, 'Обновил цены', parse_mode='Markdown')
+
+
+if __name__ == '__main__':
+	# main()
+	app.run()
