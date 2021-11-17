f20a30d824c94a48379dc11dd43325a79c24040e
zero-nowhere <52384715+zero-nowhere@users.noreply.github.com>
2021-11-17 16:53:57 +0100

    Still trying to get updates automatically.

--- clock.py
+++ clock.py
@@ -1,10 +1,8 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from main import bot
-from getprices import *
+import getprices
 
 beer_list = ['branik', 'svijany', 'gambrinus']
-for beer in beer_list:
-    parse_beer(beer)
 
 
 sched = BlockingScheduler()
@@ -12,7 +10,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', day='*', hour=7)
 def scheduled_job():
     for beer in beer_list:
-        parse_beer(beer)
+        getprices.parse_beer(beer)
     bot.send_message(203013998, 'Updated prices')
     print('This job is run every day at 7.')
 
